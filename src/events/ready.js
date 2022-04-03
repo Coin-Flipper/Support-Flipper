a6ea30bb61d986c8c8ba239df34c643e967920f7
Liam Skinner <liamdavidskinner@gmail.com>
2022-04-03 17:44:04 +0100

    refactor: removed all commands

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const { Collection } = require('discord.js');
-
 module.exports = {
 	name: "ready",
 	once: true,
@@ -17,25 +14,6 @@ module.exports = {
 		});
 
 		console.log(`Last restart: ${startTime}\n\nLogged in as ${client.user.tag}! looking over ${servers} servers`);
-
-		/* Registering slash commands */
-		client.commands = new Collection();
-		const data = [];
-
-		const categories = fs.readdirSync(`${__dirname}/../commands/`);
-		for (const category of categories) {
-			const commandFiles = fs.readdirSync(`${__dirname}/../commands/${category}`).filter(file => file.endsWith('.js'));
-			for (const file of commandFiles) {
-
-				const command = require(`${__dirname}/../commands/${category}/${file}`);
-				client.commands.set(command.name, command);
-				data.push(command);
-
-			}
-		}
-
-		/* Set ApplicationCommand data */
-		await client.application.commands.set(data);
 
 	}
 };
